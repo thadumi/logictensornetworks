5a031e04786115e7e403aa2f8bae315a0db4d2b4
thadumi <dumitrescu_theodor_a_@hotmail.it>
2019-10-28 10:19:16 +0100

    smokes friends test with LTF in TF 2.0 with keras optimizer

--- examples_ltn/smokes_friends_cancer.py
+++ examples_ltn/smokes_friends_cancer.py
@@ -87,40 +87,12 @@ optimizer = tf.keras.optimizers.RMSprop(learning_rate=.01, decay=.9)
 
 
 # Iterate over the batches of the dataset.
-for step in range(10):
-
-    # Open a GradientTape to record the operations run
-    # during the forward pass, which enables autodifferentiation.
-    with tf.GradientTape() as tape:
-        facts = [Friends(g[x], g[y]) for (x, y) in friends] + \
-                [Not(Friends(g[x], g[y])) for x in g1 for y in g1
-                 if (x, y) not in friends and x < y] + \
-                [Not(Friends(g[x], g[y])) for x in g2 for y in g2
-                 if (x, y) not in friends and x < y] + \
-                [Smokes(g[x]) for x in smokes] + \
-                [Not(Smokes(g[x])) for x in g if x not in smokes] + \
-                [Cancer(g[x]) for x in cancer] + \
-                [Not(Cancer(g[x])) for x in g1 if x not in cancer] + \
-                [Forall(p, Not(Friends(p, p))),
-                 Forall((p, q), Equiv(Friends(p, q), Friends(q, p))),
-                 Equiv(Forall(p1, Implies(Smokes(p1), Cancer(p1))),
-                       Forall(p2, Implies(Smokes(p2), Cancer(p2)))),
-                 Equiv(Forall(p1, Implies(Cancer(p1), Smokes(p1))),
-                       Forall(p2, Implies(Cancer(p2), Smokes(p2))))]
-
-        loss_value = -(1.0 / tf.reduce_mean(1 / tf.concat(facts, axis=0))) + ltn.BIAS
-
-    # Use the gradient tape to automatically retrieve
-    # the gradients of the trainable variables with respect to the loss.
-    grads = tape.gradient(loss_value, variables)
-
-    # Run one step of gradient descent by updating
-    # the value of the variables to minimize the loss.
-    optimizer.apply_gradients(zip(grads, variables))
+for step in range(10000):
+    optimizer.minimize(loss, var_list=lambda: variables, )
 
     # Log every 200 batches.
     if step % 100 == 0:
-        print(step, "=====>", loss_value, ltn.BIAS)
+        print(step, "=====>", float(ltn.BIAS))
 
 df_smokes_cancer = pd.DataFrame(tf.concat([Smokes(p), Cancer(p)], axis=1).numpy(),
                                 columns=["Smokes", "Cancer"],
@@ -141,45 +113,17 @@ plt.subplot(133)
 plt_heatmap(df_friends_in)
 plt.show()
 
-''''
-with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for i in range(10000):
-                sess.run(optimize)
-                if i % 100 == 0:
-                    print(i,"=====>",sess.run(loss),sess.run(ltn.BIAS))
-        df_smokes_cancer = pd.DataFrame(sess.run(tf.concat([Smokes(p),Cancer(p)],axis=1)),
-                           columns=["Smokes","Cancer"],
-                           index=list('abcdefghijklmn'))
-        pred_friends = sess.run(tf.squeeze(Friends(p,q)))
-        df_friends_ah = pd.DataFrame(pred_friends[:8,:8],
-                           index=list('abcdefgh'),
-                           columns=list('abcdefgh'))
-        df_friends_in = pd.DataFrame(pred_friends[8:,8:],
-                           index=list('ijklmn'),
-                           columns=list('ijklmn'))
-        plt.figure(figsize=(17,5))
-        plt.subplot(131)
-        plt_heatmap(df_smokes_cancer)
-        plt.subplot(132)
-        plt_heatmap(df_friends_ah)
-        plt.subplot(133)
-        plt_heatmap(df_friends_in)
-        plt.show()
-
-        print("forall x ~Friends(x,x)",
-              sess.run(Forall(p,Not(Friends(p,p)))))
-        print("Forall x Smokes(x) -> Cancer(x)",
-              sess.run(Forall(p,Implies(Smokes(p),Cancer(p)))))
-        print("forall x y Friends(x,y) -> Friends(y,x)",
-              sess.run(Forall((p,q),Implies(Friends(p,q),Friends(q,p)))))
-        print("forall x Exists y (Friends(x,y)",
-              sess.run(Forall(p,Exists(q,Friends(p,q)))))
-        print("Forall x,y Friends(x,y) -> (Smokes(x)->Smokes(y))",
-              sess.run(Forall((p,q),Implies(Friends(p,q),Implies(Smokes(p),Smokes(q))))))
-        print("Forall x: smokes(x) -> forall y: friend(x,y) -> smokes(y))",
-              sess.run(Forall(p,Implies(Smokes(p),
-                                        Forall(q,Implies(Friends(p,q),
-                                                         Smokes(q)))))))
-
-'''
+print("forall x ~Friends(x,x)",
+      (Forall(p, Not(Friends(p, p)))))
+print("Forall x Smokes(x) -> Cancer(x)",
+      (Forall(p, Implies(Smokes(p), Cancer(p)))))
+print("forall x y Friends(x,y) -> Friends(y,x)",
+      (Forall((p, q), Implies(Friends(p, q), Friends(q, p)))))
+print("forall x Exists y (Friends(x,y)",
+      (Forall(p, Exists(q, Friends(p, q)))))
+print("Forall x,y Friends(x,y) -> (Smokes(x)->Smokes(y))",
+      (Forall((p, q), Implies(Friends(p, q), Implies(Smokes(p), Smokes(q))))))
+print("Forall x: smokes(x) -> forall y: friend(x,y) -> smokes(y))",
+      (Forall(p, Implies(Smokes(p),
+                                 Forall(q, Implies(Friends(p, q),
+                                                   Smokes(q)))))))
