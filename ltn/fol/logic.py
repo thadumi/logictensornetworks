a09d3239f436670ad21c01238f40cf61210da799
thadumi <dumitrescu_theodor_a_@hotmail.it>
2019-11-26 11:36:35 +0100

    Added the TF logic foreach Logical Operation

--- ltn/fol/logic.py
+++ ltn/fol/logic.py
@@ -6,7 +6,8 @@
 
 import tensorflow as tf
 
-from ltn_utils import cross_args, cross_2args
+from ltn.backend.ltn_utils import cross_args, cross_2args
+from ltn.backend.norms import F_And, F_Or, F_Not, F_Implies, F_Equiv, F_Exists, F_ForAll
 
 
 class LogicalComputation(object):
@@ -47,7 +48,8 @@ class LogicalComputation(object):
     def __and__(self, other):
         print(str(self) + ' and ' + str(other))
         tensor_cross_args, result_doms = cross_args([self._out_doms, other.doms])
-        return AndLogicalComputation(in_doms=[self.doms, other.doms], out_doms=result_doms, args=[self, other])
+        return AndLogicalComputation(in_doms=[self.doms, other.doms], out_doms=result_doms,
+                                     args=[self, other], tensor_cross_args=tensor_cross_args)
 
     def or_(self, other):
         return self.__or__(other)
@@ -57,22 +59,25 @@ class LogicalComputation(object):
 
     def __or__(self, other):
         tensor_cross_args, result_doms = cross_args([self._out_doms, other.doms])
-        return OrLogicalComputation(in_doms=[self.doms, other.doms], out_doms=result_doms, args=[self, other])
+        return OrLogicalComputation(in_doms=[self.doms, other.doms], out_doms=result_doms,
+                                    args=[self, other], tensor_cross_args=tensor_cross_args)
 
     def not_(self):
         return self.__invert__()
 
     def __invert__(self):
-        return NotLogicalComputation(self.doms, self.doms, self)
+        return NotLogicalComputation(self.doms, self.doms, [self])
 
     def __rshift__(self, other):
         eX_doms, _, _, tensor_cross_2args = cross_2args(x_ltn_doms=self.doms, y_ltn_doms=other.doms)
-        return ImpliesLogicalComputation(in_doms=[self.doms, other.doms], out_doms=eX_doms, args=[self, other])
+        return ImpliesLogicalComputation(in_doms=[self.doms, other.doms], out_doms=eX_doms,
+                                         args=[self, other], tensor_cross_args=tensor_cross_2args)
 
     def __eq__(self, other):
         # WARN overriding __eq__ could cause issues on dictionaries
         eX_doms, _, _, tensor_cross_2args = cross_2args(x_ltn_doms=self.doms, y_ltn_doms=other.doms)
-        return EquivalenceLogicalComputation(in_doms=[self.doms, other.doms], out_doms=eX_doms, args=[self, other])
+        return EquivalenceLogicalComputation(in_doms=[self.doms, other.doms], out_doms=eX_doms,
+                                             args=[self, other], tensor_cross_args=tensor_cross_2args)
 
     def __hash__(self):
         # needed by tf.function for hashing the self argument in logical predicate name
@@ -86,16 +91,26 @@ class LogicalComputation(object):
 # this could be done via __rand__ (the operations are aggregated from lest to right)
 
 class AndLogicalComputation(LogicalComputation):
-    def __init__(self, in_doms, out_doms, args=None):
+    def __init__(self, in_doms, out_doms, args, tensor_cross_args):
         super(AndLogicalComputation, self).__init__(in_doms, out_doms, args)
+        self.tensor_cross_args = tensor_cross_args
+
+    def _compute(self, args):
+        cross_wffs, _ = self.tensor_cross_args(args)
+        return F_And(cross_wffs)
 
     def __str__(self):
         return ' ∧ '.join([str(arg) for arg in self._ltn_args])
 
 
 class OrLogicalComputation(LogicalComputation):
-    def __init__(self, in_doms, out_doms, args=None):
+    def __init__(self, in_doms, out_doms, args, tensor_cross_args):
         super(OrLogicalComputation, self).__init__(in_doms, out_doms, args)
+        self.tensor_cross_args = tensor_cross_args
+
+    def _compute(self, args):
+        cross_wffs, _ = self.tensor_cross_args(args)
+        return F_Or(cross_wffs)
 
     def __str__(self):
         return ' ∨ '.join([str(arg) for arg in self._ltn_args])
@@ -105,21 +120,34 @@ class NotLogicalComputation(LogicalComputation):
     def __init__(self, in_doms, out_doms, args):
         super(NotLogicalComputation, self).__init__(in_doms, out_doms, args)
 
+    def _compute(self, args):
+        return F_Not(args[0])
+
     def __str__(self):
         return '¬' + str(self._ltn_args)
 
 
 class ImpliesLogicalComputation(LogicalComputation):
-    def __init__(self, in_doms, out_doms, args):
+    def __init__(self, in_doms, out_doms, args, tensor_cross_args):
         super(ImpliesLogicalComputation, self).__init__(in_doms, out_doms, args)
+        self.tensor_cross_args = tensor_cross_args
+
+    def _compute(self, args):
+        _, cross_wffs = self.tensor_cross_args(args[0], args[1])
+        return F_Implies(cross_wffs[0], cross_wffs[1])
 
     def __str__(self):
         return str(self._ltn_args[0]) + ' ⇒ ' + str(self._ltn_args[1])
 
 
 class EquivalenceLogicalComputation(LogicalComputation):
-    def __init__(self, in_doms, out_doms, args):
+    def __init__(self, in_doms, out_doms, args, tensor_cross_args):
         super(EquivalenceLogicalComputation, self).__init__(in_doms, out_doms, args)
+        self.tensor_cross_args = tensor_cross_args
+
+    def _compute(self, args):
+        _, cross_wffs = self.tensor_cross_args(args[0], args[1])
+        return F_Equiv(cross_wffs[0], cross_wffs[1])
 
     def __str__(self):
         return str(self._ltn_args[0]) + ' ⇔ ' + str(self._ltn_args[1])
@@ -130,6 +158,18 @@ class ForAllLogicalComputation(LogicalComputation):
         super(ForAllLogicalComputation, self).__init__(in_doms, out_doms, args=[*variables, proposition])
         self.vars = variables
         self.proposition = proposition
+        self.quantif_axis = [proposition.doms.index(var.doms[0]) for var in variables]
+
+    def _compute(self, args):
+        vars = args[:-1]
+        wff = args[-1]
+
+        # not_empty_vars = tf.cast(tf.math.reduce_prod(tf.stack([tf.size(var) for var in vars])),
+        #                         dtype=tf.dtypes.bool)
+        # if not_empty_vars:
+        return F_ForAll(self.quantif_axis, wff)
+        # else:
+        #    return tf.ones((1,) * (len(self.doms) + 1))
 
     def __str__(self):
         return '∀ ' + ','.join([str(var) for var in self.vars]) + ': ' + str(self.proposition)
@@ -140,6 +180,18 @@ class ExistsLogicalComputation(LogicalComputation):
         super(ExistsLogicalComputation, self).__init__(in_doms, out_doms, args=[*variables, proposition])
         self.vars = variables
         self.proposition = proposition
+        self.quantif_axis = [proposition.index(var.doms[0]) for var in variables]
+
+    def _compute(self, args):
+        vars = args[:-1]
+        wff = args[-1]
+
+        # not_empty_vars = tf.cast(tf.math.reduce_prod(tf.stack([tf.size(var) for var in vars])),
+        #                         dtype=tf.dtypes.bool)
+        # if not_empty_vars:
+        return F_Exists(self.quantif_axis, wff)
+        # else:
+        #    return tf.ones((1,) * (len(self.doms) + 1))
 
     def __str__(self):
         return '∃ ' + ','.join([str(var) for var in self.vars]) + ': ' + str(self.proposition)
@@ -151,16 +203,21 @@ def Not(lc: LogicalComputation) -> LogicalComputation:
 
 def Forall(variables, proposition: LogicalComputation):
     # TODO(thadumi) assert variables and proposition type
+    # TODO(thadumi) add dock for Forlall functional API
 
     if type(variables) is not list or type(variables) is not tuple:
         variables = (variables,)
 
     result_doms = [x for x in proposition.doms if x not in [var.doms[0] for var in variables]]
+
     return ForAllLogicalComputation([*[var.doms for var in variables], proposition.doms], result_doms,
                                     variables, proposition)
 
 
-def Exist(variables, proposition: LogicalComputation):
+def Exist(variables, proposition):
+    # TODO(thadumi) assert variables and proposition type
+    # TODO(thadumi) add dock for Exist functional API
+
     if type(variables) is not list or type(variables) is not tuple:
         variables = (variables,)
 
